8423bd26b52295de7daca83f8b3ae32e4ed8a4d4
beemwe@gmx.at <beemwe@gmx.at@43b04e41-3fed-2a63-d2e4-a62c9e7fa92f>
2011-10-20 20:47:22 +0000

    initial checkin

---
+++ trunk/Thanoma/v0.1/Thanoma/Thanoma/Game1.cs
@@ -0,0 +1,184 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Thanoma
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteBatch spriteBatch2;
+
+        Texture2D bg;
+        public Rectangle rect_bg;
+
+        int start_x = 0;
+        int start_x2 = 0;
+
+        Player test_player;
+        Level test_level;
+
+        private Camera2D camera = new Camera2D();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            ContentManager cm = Content;
+            test_player = new Player(cm, 0);
+            test_level = new Level();
+
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            bg = Content.Load<Texture2D>("bg1");
+            rect_bg.Width = bg.Width;
+            rect_bg.Height = bg.Height;
+
+            test_player._texture = Content.Load<Texture2D>("player1");
+            
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) == true)
+                this.Exit();
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) == true)
+            {
+                //rect_bg.X -= 2;
+                //start_x -= 2;
+                start_x2 += 2;
+                test_player._rect.X += 2;
+                
+            }
+            if ((Keyboard.GetState().IsKeyDown(Keys.Right) == true) && (Keyboard.GetState().IsKeyDown(Keys.Space) == true))
+            {
+                //rect_bg.X -= 3;
+                //start_x -= 4;
+                start_x2 += 3;
+                test_player._rect.X += 3;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) == true)
+            {
+                //rect_bg.X += 2;
+                //start_x += 2;
+                start_x2 -= 2;
+                test_player._rect.X -= 2;
+            }
+
+            // TODO: Add your update logic here
+            
+            this.camera.Update(gameTime, new Vector2(start_x2, 0));
+            base.Update(gameTime);
+        }
+
+        bool drawn = true;
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.PreferredBackBufferWidth = VaC.WINDOW_WIDTH;
+            graphics.PreferredBackBufferHeight = VaC.WINDOW_HEIGHT;
+            graphics.ApplyChanges();
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            
+
+            spriteBatch.Begin(SpriteBlendMode.AlphaBlend, SpriteSortMode.Immediate, SaveStateMode.None, this.camera.getMatrix());
+            spriteBatch.Draw(bg, rect_bg, Color.White);
+            
+            // TEST LEVEL
+            
+            ContentManager cm = Content;
+            test_level.BuildTestLevel(cm, spriteBatch, start_x);
+
+            // TEST PLAYER
+            //Player test_player = new Player(ref cm, 0);
+            PlayMoveAnimation(test_player, gameTime);
+            spriteBatch.Draw(test_player._texture, test_player._rect, Color.White);
+            
+            spriteBatch.End();
+            
+
+            // TODO: Add your drawing code here
+            
+            base.Draw(gameTime);
+        }
+        bool change = false;
+        TimeSpan ticks = new TimeSpan(0);
+        public void PlayMoveAnimation(Player player, GameTime gt)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) == true)
+            {
+                if (gt.TotalGameTime > ticks)
+                {
+                    if (change == true)
+                    {
+                        player._texture = Content.Load<Texture2D>("player1");
+                        change = false;
+                    }
+                    else
+                    {
+                        player._texture = Content.Load<Texture2D>("player1b");
+                        change = true;
+                    }
+                    ticks = gt.TotalGameTime + new TimeSpan(0, 0, 0, 0, 200);
+                }
+            }
+        }
+    }
+}
---
+++ trunk/Thanoma/v0.1/Thanoma/Thanoma/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Thanoma
+{
+    public class Player
+    {
+        int _type;
+        public Texture2D _texture = null;
+        public Rectangle _rect;
+
+        public Player(ContentManager cm, int type)
+        {
+            _type = type;
+
+            _rect.Width = VaC.BRICK_WIDTH;
+            _rect.Height = VaC.BRICK_HEIGHT;
+            _rect.X = VaC.WINDOW_WIDTH / 2;
+            _rect.Y = VaC.WINDOW_HEIGHT - VaC.BRICK_HEIGHT * 6;
+
+            switch (type)
+            {
+                case 0:
+                    // test guy
+                    //_texture = cm.Load<Texture2D>("player1");
+                    break;
+                default:
+                    // test guy
+                    //_texture = cm.Load<Texture2D>("player1");
+                    break;
+            }
+        }
+
+    }
+}
