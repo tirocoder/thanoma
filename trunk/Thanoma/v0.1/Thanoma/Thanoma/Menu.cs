96f6b88984f92bf080d4f36103088ef167d41866
beemwe@gmx.at <beemwe@gmx.at@43b04e41-3fed-2a63-d2e4-a62c9e7fa92f>
2011-11-07 21:06:06 +0000

    sdfsfsdfsfsdfsdff

---
+++ trunk/Thanoma/v0.1/Thanoma/Thanoma/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Thanoma
+{
+    public class Menu
+    {
+        /* START: properties */
+
+        Texture2D _background;
+        Rectangle _rect_background;
+
+        /* END: properties */
+
+
+        // constructor
+        public Menu()
+        {
+            
+        }
+
+        /* START: methods */
+
+
+
+        /* END: methods */
+    }
+}
