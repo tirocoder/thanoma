44b9ac7ac890919915b615834f325ec213be6b27
beemwe@gmx.at <beemwe@gmx.at@43b04e41-3fed-2a63-d2e4-a62c9e7fa92f>
2011-11-17 19:29:30 +0000

    Character-Klasse erweitert

--- Thanoma/v0.1/Thanoma/Thanoma/Character.cs
+++ Thanoma/v0.1/Thanoma/Thanoma/Character.cs
@@ -25,15 +25,16 @@ namespace Thanoma
     {
         /* START: properties */
 
-        int _type;
-        Direction _direction;
-        double _speed;
+        protected int _type;
+        protected Direction _direction;
+        protected double _speed;
 
         protected int _x;
         protected int _y;
         protected int _brick_x;
         protected int _brick_y;
 
+        protected string[] _textures = new string[4];
         public Texture2D _texture = null;
         public Rectangle _rect;
         
@@ -64,6 +65,10 @@ namespace Thanoma
                 case 0:
                     // test guy
                     _texture = cm.Load<Texture2D>(VaC.TEXTURE_PLAYER_RIGHT1);
+                    _textures[0] = VaC.TEXTURE_PLAYER_LEFT1;
+                    _textures[1] = VaC.TEXTURE_PLAYER_LEFT2;
+                    _textures[2] = VaC.TEXTURE_PLAYER_RIGHT1;
+                    _textures[3] = VaC.TEXTURE_PLAYER_RIGHT2;
                     break;
                 default:
                     // test guy
@@ -111,10 +116,124 @@ namespace Thanoma
             }
         }
 
-        public void Jump()
+        /// <summary>
+        /// animates the character while walking right/left
+        /// </summary>
+        bool change = false;
+        TimeSpan ticks = new TimeSpan(0);
+        public void PlayMoveAnimation(ContentManager cm, GameTime gt)
         {
-
+            if (Keyboard.GetState().IsKeyDown(Keys.D) == true)
+            {
+                if (gt.TotalGameTime > ticks)
+                {
+                    if (change == true)
+                    {
+                        _texture = cm.Load<Texture2D>(_textures[2]);
+                        change = false;
+                    }
+                    else
+                    {
+                        _texture = cm.Load<Texture2D>(_textures[3]);
+                        change = true;
+                    }
+                    ticks = gt.TotalGameTime + new TimeSpan(0, 0, 0, 0, Convert.ToInt32(200 / _speed));
+                }
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A) == true)
+            {
+                if (gt.TotalGameTime > ticks)
+                {
+                    if (change == true)
+                    {
+                        _texture = cm.Load<Texture2D>(_textures[0]);
+                        change = false;
+                    }
+                    else
+                    {
+                        _texture = cm.Load<Texture2D>(_textures[1]);
+                        change = true;
+                    }
+                    ticks = gt.TotalGameTime + new TimeSpan(0, 0, 0, 0, Convert.ToInt32(200 / _speed));
+                }
+            }
         }
+
+        /// <summary>
+        /// character jumps while user presses and holds jump key
+        /// </summary>
+        int start_y;
+        bool is_jumping = false;
+        public void Jump1(Level level)
+        {
+            int value = level.IsBrickAboveMe(_brick_x, _brick_y, _x, _y);
+            if ((value != 0) && (10 <= value))
+            {
+                if (_y >= start_y - 56)
+                {
+                    is_jumping = true;
+                    _y -= 10;
+                }
+                else is_jumping = false;
+            }
+            else //((value != 0) && (10 >= value))
+            {
+                _y += value;
+            }
+        }
+
+        /// <summary>
+        /// makes character follow gravity
+        /// </summary>
+        double val = 0.0;
+        public void FollowGravity(Level level)
+        {
+            int value = level.IsBrickUnderMe(_brick_x, _brick_y, _x, _y);
+            if ((value != 0) && (2 + (int)val <= value))
+            {
+                // let player fall down (if not on ground)
+                _y += 2 + (int)val;
+                val += 0.3;
+            }
+            else if ((value != 0) && (2 + (int)val >= value))
+            {
+                _y += value;
+            }
+            else val = 0.0;
+        }
+
+
+
+
+
+        /// <summary>
+        /// declares rectangle of character for drawing
+        /// </summary>
+        public void DeclareRectangle()
+        {
+            _rect.Width = VaC.PLAYER_WIDTH;
+            _rect.Height = VaC.PLAYER_HEIGHT;
+            _rect.X = _x;
+            _rect.Y = _y;
+        }
+
+        /// <summary>
+        /// updates the character
+        /// </summary>
+        /// <param name="cm"></param>
+        /// <param name="gametime"></param>
+        /// <param name="level"></param>
+        /// <param name="sb"></param>
+        /// <param name="is_player"></param>
+        public void Update(ContentManager cm, GameTime gametime, Level level, SpriteBatch sb)
+        {
+            PlayMoveAnimation(cm, gametime);
+            _brick_x = (int)((double)_x / VaC.BRICK_WIDTH);
+            _brick_y = (int)((double)_y / VaC.BRICK_HEIGHT);
+            FollowGravity(level);
+            DeclareRectangle();
+        }
+
 
         /* END: methods */
     }
